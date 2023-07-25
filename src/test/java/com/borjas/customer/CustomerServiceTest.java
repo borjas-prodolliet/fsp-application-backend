3d595a87585c6a57e2feb700771ee06eab4badf6
Borjas Prodolliet <borjas.prodolliet@gmail.com>
2023-07-25 15:02:22 -0300

    Testing CustomerService

--- src/test/java/com/borjas/customer/CustomerServiceTest.java
+++ src/test/java/com/borjas/customer/CustomerServiceTest.java
@@ -1,9 +1,11 @@
 package com.borjas.customer;
 
+import com.borjas.exeption.DuplicateResourceException;
 import com.borjas.exeption.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -11,8 +13,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -70,19 +71,77 @@ class CustomerServiceTest {
     @Test
     void addCustomer() {
         // Given
+        String email = "alex@gmail.com";
+
+        when(customerDao.existsCustomerWithEmail(email)).thenReturn(false);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+                "Alex", email, 18
+        );
 
         // When
+        underTest.addCustomer(request);
 
         // Then
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+
+        verify(customerDao).insertCustomer(customerArgumentCaptor.capture());
+
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+
+        assertThat(capturedCustomer.getId()).isNull();
+        assertThat(capturedCustomer.getName()).isEqualTo(request.name());
+        assertThat(capturedCustomer.getEmail()).isEqualTo(request.email());
+        assertThat(capturedCustomer.getAge()).isEqualTo(request.age());
+    }
+
+    @Test
+    void willThrowWhenEmailExistsWhileAddingACustomer() {
+        // Given
+        String email = "alex@gmail.com";
+
+        when(customerDao.existsCustomerWithEmail(email)).thenReturn(true);
+
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+                "Alex", email, 18
+        );
+
+        // When
+        assertThatThrownBy(() -> underTest.addCustomer(request))
+                .isInstanceOf(DuplicateResourceException.class)
+                .hasMessage("email already taken");
+
+
+        // Then
+        verify(customerDao, never()).insertCustomer(any());
     }
 
     @Test
     void deleteCustomerById() {
         // Given
+        var id = 1L;
+        when(customerDao.existsCustomerWithId(id)).thenReturn(true);
 
         // When
+        underTest.deleteCustomerById(id);
 
         // Then
+        verify(customerDao).deleteCustomerById(id);
+    }
+
+    @Test
+    void willThrownWhenCustomerNotExistsWhileDeletingCustomerById() {
+        // Given
+        var id = 1L;
+        when(customerDao.existsCustomerWithId(id)).thenReturn(false);
+
+        // When
+        assertThatThrownBy(() -> underTest.deleteCustomerById(id))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("customer with id [%s] not found".formatted(id));
+
+        // Then
+        verify(customerDao, never()).deleteCustomerById(id);
     }
 
     @Test
